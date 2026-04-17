7f0005acc952cf651e75de39dd386c169f7e119c
Felix Isael Mejia Castillo <isaelmejia06@gmail.com>
2026-04-17 19:07:42 -0400

    Creando el ejercicio y añadiendo el modelo

---
+++ Registro_de_Estudiantes/Models/Estudiante.cs
@@ -0,0 +1,21 @@
+﻿namespace Registro_de_Estudiantes.Models
+{
+    public class Estudiante
+    {
+        public int ID { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+
+        public int ObtenerEdad()
+        {
+            var hoy = DateTime.Today;
+            int edad = hoy.Year - FechaNacimiento.Year;
+            if (FechaNacimiento.Date > hoy.AddYears(-edad))
+                edad--;
+            return edad;
+        }
+    }
+}
