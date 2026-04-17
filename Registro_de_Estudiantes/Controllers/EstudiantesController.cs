b0e9488ef32861cf32f0562497a5abf3d8059a09
Felix Isael Mejia Castillo <isaelmejia06@gmail.com>
2026-04-17 19:17:00 -0400

    Agregando DbContext y el Controlador

---
+++ Registro_de_Estudiantes/Controllers/EstudiantesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Registro_de_Estudiantes.Data;
+using Registro_de_Estudiantes.Models;
+
+namespace Registro_de_Estudiantes.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EstudiantesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public EstudiantesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear(Estudiante estudiante)
+        {
+            _context.Estudiantes.Add(estudiante);
+            await _context.SaveChangesAsync();
+            return Ok(estudiante);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Obtener()
+        {
+            var lista = await _context.Estudiantes
+                .Select(e => new {
+                    e.Nombres,
+                    e.Apellidos,
+                    Edad = e.ObtenerEdad(),
+                    e.Direccion,
+                    e.Telefono
+                })
+                .ToListAsync();
+
+            return Ok(lista);
+        }
+    }
+}
---
+++ Registro_de_Estudiantes/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Registro_de_Estudiantes.Models;
+
+namespace Registro_de_Estudiantes.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Estudiante> Estudiantes { get; set; }
+    }
+}
